cb1b43948aa46663d0cafa6657162f8f039aa47b
yusto <yuusufmahud@gmail.com>
2024-10-22 21:51:10 +0300

    fix asyncHandler

--- backend/utils/asynchandler.js
+++ backend/utils/asynchandler.js
@@ -1,3 +1,2 @@
-export const asyncHandler = (fn) => (req, res, next) => {
-  return Promise.resolve(fn(req, res, next)).catch(next);
-};
+export const asyncHandler = (fn) => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
